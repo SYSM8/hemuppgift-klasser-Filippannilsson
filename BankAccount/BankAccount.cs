4a03c6dab0ddc70f07b45d1f5fc4037fc88121b8
Filippannilsson <filippanilsson95@gmail.com>
2024-09-20 09:24:47 +0200

    Added constructor

--- BankAccount/BankAccount.cs
+++ BankAccount/BankAccount.cs
@@ -11,9 +11,16 @@ namespace BankAccount
         //Lägg till Egenskaper (fields)
         public string AccountNumber { get; set; }
         public string AccountHolder { get; set; }
-        public double Balance { get; set; }
+        public decimal Balance { get; set; }
 
         //Lägg till Konstruktor
+
+        public BankAccount (string accountnumber, string accountholder, decimal balance)
+        {
+            AccountNumber = accountnumber;
+            AccountHolder = accountholder;
+            Balance = balance;
+        }
 
         //Lägg till Metoder
 
