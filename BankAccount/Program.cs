b2be0ece87dad87a01506eb79e359d6842d6cfda
Filippannilsson <filippanilsson95@gmail.com>
2024-09-20 09:55:28 +0200

    Added BankAccount objects and a list to store them

--- BankAccount/Program.cs
+++ BankAccount/Program.cs
@@ -5,7 +5,21 @@
         static void Main(string[] args)
         {
             //Instansiera bankAccount klassen och testa den
-            Console.WriteLine("Test");
+            //Skapa ett BankAccount-objekt
+            BankAccount account1 = new BankAccount("12345678", "Filippa Nilsson", 20000);
+            BankAccount account2 = new BankAccount("87654321", "Linnea Hansson", 30000);
+            BankAccount account3 = new BankAccount("24681357", "Linn Olsson", 100000);
+            BankAccount account4 = new BankAccount("13572468", "Lova Gustavsson", 15000);
+
+
+            //Skapa en lista med alla konton
+            List<BankAccount> accountList = new List<BankAccount>
+            {
+                account1,
+                account2,
+                account3,
+                account4
+            };
         }
     }
 }
